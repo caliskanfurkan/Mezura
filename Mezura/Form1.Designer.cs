b8189daca18baffb811caa1c0cfecd4415ab179a
Furkan ÇALIŞKAN <caliskanfurkan@gmail.com>
2015-02-07 18:38:26 +0200

    ilk

---
+++ Mezura/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Mezura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnEval = new System.Windows.Forms.Button();
+            this.tabPanel = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.tabPanel.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(106, 30);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(216, 20);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "http://";
+            // 
+            // btnEval
+            // 
+            this.btnEval.Location = new System.Drawing.Point(347, 27);
+            this.btnEval.Name = "btnEval";
+            this.btnEval.Size = new System.Drawing.Size(75, 23);
+            this.btnEval.TabIndex = 1;
+            this.btnEval.Text = "Değerlendir";
+            this.btnEval.UseVisualStyleBackColor = true;
+            this.btnEval.Click += new System.EventHandler(this.btnEval_Click);
+            // 
+            // tabPanel
+            // 
+            this.tabPanel.Controls.Add(this.tabPage1);
+            this.tabPanel.Controls.Add(this.tabPage2);
+            this.tabPanel.Location = new System.Drawing.Point(23, 55);
+            this.tabPanel.Name = "tabPanel";
+            this.tabPanel.SelectedIndex = 0;
+            this.tabPanel.Size = new System.Drawing.Size(491, 292);
+            this.tabPanel.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtReport);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 266);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(483, 266);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(0, 0);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(483, 266);
+            this.txtReport.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 359);
+            this.Controls.Add(this.tabPanel);
+            this.Controls.Add(this.btnEval);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Mezura v0.1b";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPanel.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnEval;
+        private System.Windows.Forms.TabControl tabPanel;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Mezura/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mezura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEval_Click(object sender, EventArgs e)
+        {
+            tabPanel.Enabled = true;
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(txtURL.Text);
+            myRequest.Method = "GET";
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            WebHeaderCollection myHeaders = myResponse.Headers;
+            txtReport.Text = myHeaders.ToString();
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabPanel.Enabled = false;
+            tabPage1.Text = "Başlık Bilgileri";
+            tabPage2.Text = "Port Tarama";
+        }
+    }
+}
