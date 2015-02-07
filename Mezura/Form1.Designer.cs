f8ea5148d7c9460277602c262dde690c6e5c18c2
Furkan ÇALIŞKAN <caliskanfurkan@gmail.com>
2015-02-08 01:47:59 +0200

    TUM

--- Mezura/Form1.Designer.cs
+++ Mezura/Form1.Designer.cs
@@ -32,8 +32,9 @@
             this.btnEval = new System.Windows.Forms.Button();
             this.tabPanel = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.txtReport = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.tabPanel.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -44,7 +45,7 @@
             this.txtURL.Name = "txtURL";
             this.txtURL.Size = new System.Drawing.Size(216, 20);
             this.txtURL.TabIndex = 0;
-            this.txtURL.Text = "http://";
+            this.txtURL.Text = "http://php.vulnweb.com";
             // 
             // btnEval
             // 
@@ -59,11 +60,10 @@
             // tabPanel
             // 
             this.tabPanel.Controls.Add(this.tabPage1);
-            this.tabPanel.Controls.Add(this.tabPage2);
-            this.tabPanel.Location = new System.Drawing.Point(23, 55);
+            this.tabPanel.Location = new System.Drawing.Point(23, 112);
             this.tabPanel.Name = "tabPanel";
             this.tabPanel.SelectedIndex = 0;
-            this.tabPanel.Size = new System.Drawing.Size(491, 292);
+            this.tabPanel.Size = new System.Drawing.Size(491, 215);
             this.tabPanel.TabIndex = 3;
             // 
             // tabPage1
@@ -72,34 +72,42 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(483, 266);
+            this.tabPage1.Size = new System.Drawing.Size(483, 189);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(483, 266);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // txtReport
             // 
-            this.txtReport.Location = new System.Drawing.Point(0, 0);
+            this.txtReport.Location = new System.Drawing.Point(0, 3);
             this.txtReport.Multiline = true;
             this.txtReport.Name = "txtReport";
-            this.txtReport.Size = new System.Drawing.Size(483, 266);
+            this.txtReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReport.Size = new System.Drawing.Size(483, 183);
             this.txtReport.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 60);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(220, 94);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(526, 359);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.tabPanel);
             this.Controls.Add(this.btnEval);
             this.Controls.Add(this.txtURL);
@@ -121,7 +129,8 @@
         private System.Windows.Forms.TabControl tabPanel;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox txtReport;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- Mezura/Form1.cs
+++ Mezura/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
@@ -18,25 +20,89 @@ namespace Mezura
             InitializeComponent();
         }
 
+        List<string> linkler;
+
+ 
         private void btnEval_Click(object sender, EventArgs e)
         {
-            tabPanel.Enabled = true;
 
-            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(txtURL.Text);
-            myRequest.Method = "GET";
-            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
-            WebHeaderCollection myHeaders = myResponse.Headers;
-            txtReport.Text = myHeaders.ToString();
             
+            txtReport.Clear();
 
+            tabPanel.Enabled = true;
+            lblStatus.Text = "Başlık bilgileri alınıyor";
+            this.HeaderAl();
+            lblStatus.Text = "Başlık bilgileri alındı";
+            this.LinkleriAl();
+            
+        }
+
+        private void LinkleriAl() 
+        {
+            try
+            {
+                linkler = new List<string>();
+                HtmlWeb hw = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = hw.Load(txtURL.Text);
+
+                List<string> hrefTags = new List<string>();
+                doc.DocumentNode.SelectNodes("//a[@href]");
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//a"))
+                {
+                    // Linkleri listeye atıyoruz.
+                    linkler.Add(node.GetAttributeValue("href", null));
+                }
+
+            }
+            catch 
+            {
+                this.HataBas("Linkler alınırken hata oluştu.");
+            }
+
+            foreach (string element in linkler)
+            {
+                txtReport.AppendText(element);
+                txtReport.AppendText(Environment.NewLine);
+            }
 
         }
+
+        private void HeaderAl()
+        {
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(txtURL.Text);
+                myRequest.Method = "GET";
+                HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+                WebHeaderCollection myHeaders = myResponse.Headers;
+                txtReport.Text = "Adres: " + txtURL.Text;
+                txtReport.AppendText(Environment.NewLine);
+                txtReport.AppendText(Environment.NewLine);
+                txtReport.AppendText("Başlık Bilgileri:");
+                txtReport.AppendText(Environment.NewLine);
+                txtReport.Text += myHeaders.ToString();
+                progressBar1.Value += 100;
+            }
+            catch
+            {
+                this.HataBas("URL hatalı yada erişilemiyor");
+            }
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Bileşen ayarlamaları
             tabPanel.Enabled = false;
-            tabPage1.Text = "Başlık Bilgileri";
-            tabPage2.Text = "Port Tarama";
+            tabPage1.Text = " ÖZET ;"
+          
         }
+
+        private void HataBas(string hataMetni)
+        {
+            MessageBox.Show(hataMetni, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
     }
 }
+
